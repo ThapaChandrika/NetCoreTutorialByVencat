1cc3a84ff69a494c5845df431888612f0b053808
Chandrika Thapa <chandrika.thapa@ekbana.info>
2020-02-25 17:59:24 +0545

    Add project files.

---
+++ EmployeeManagement/Controllers/DepartmentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class DepartmentController : Controller
+    {
+        public string List()
+        {
+            return "List() of department controller";
+        }
+
+        public string Details()
+        {
+            return "Details of department controller";
+        }
+    }
+}
---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+   
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [Route("/")]
+        [Route("")]
+        [Route("Index")]
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployees();
+            return View(model);
+        }
+
+        [Route("Details/{id?}")]
+        public ViewResult Details(int? id)
+        {
+            //Employee model = _employeeRepository.GetEmployee(1);
+            //ViewBag.PageTitle = "Employee Details";
+            ////ViewBag.Employee = model;
+            ////ViewData["PageTitle"] = "Employee Details";
+            ////ViewData["Employee"] = model;
+            //return View(model);
+
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id ?? 1),
+                PageTitle = "Employee Details"
+            };
+
+            return View(homeDetailsViewModel);
+        }
+    }
+}
